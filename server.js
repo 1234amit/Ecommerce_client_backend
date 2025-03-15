2bbacddc27e5596d488cca97c87ae56d68bea156
Amit @Hostologies <d3v@hostologies.com>
2025-03-15 22:48:19 +0600

    jhgjh

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin:
-      "http://localhost:5173, http://localhost:5174, http://localhost:3000, https://krishi-ghar.vercel.app, https://krishi-ghar-admin.vercel.app", // Allow requests from frontend
+      ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "https://krishi-ghar.vercel.app", "https://krishi-ghar-admin.vercel.app"], // Allow requests from frontend
     credentials: true,
   })
 );
