d6feb6d54fb0799ac4b6ca8cbfc3e8cd16081ebd
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2025-08-13 02:13:24 +0600

    dadafa

--- routes/producer/productPublicRoutes.js
+++ routes/producer/productPublicRoutes.js
@@ -2,9 +2,6 @@
 import express from "express";
 import { getProductPublic, listByProducer, listProducts } from "../../controllers/producer/productPublicController.js";
 
-// If you want to restrict to certain roles, import verifyToken + verifyRole
-// import { verifyToken } from "../../middleware/verifyToken.js";
-// import { verifyRole } from "../../middleware/verifyRole.js";
 
 const router = express.Router();
 
@@ -13,9 +10,5 @@ router.get("/", listProducts);
 router.get("/:productId", getProductPublic);
 router.get("/by-producer/:producerId", listByProducer);
 
-// Option B: ROLE-BASED (uncomment, and comment Option A)
-// router.get("/", verifyToken, verifyRole(["superSaler", "wholeSaler", "consumer"]), listProducts);
-// router.get("/:productId", verifyToken, verifyRole(["superSaler", "wholeSaler", "consumer"]), getProductPublic);
-// router.get("/by-producer/:producerId", verifyToken, verifyRole(["superSaler", "wholeSaler", "consumer"]), listByProducer);
 
 export default router;
