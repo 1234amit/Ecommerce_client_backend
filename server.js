438ad47c948520e1330bd69e30a365ac4a5831fd
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2026-04-09 00:26:20 +0600

    update

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use(
       "https://krishi-ghar-admin.vercel.app",
       "https://krishi-test-frontend.vercel.app",
       "https://ecom-krishi-test.vercel.app",
-      "https://admin.krishighar.com/"
+      "https://admin.krishighar.com"
     ],
     credentials: true, // Allow cookies
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
